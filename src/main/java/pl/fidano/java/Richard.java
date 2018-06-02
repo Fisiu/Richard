3b55acc4875645e0157ef199b84ace784aa41438
Mariusz Fik <mariusz@fidano.pl>
2018-06-02 19:51:04 +0200

    Bot intro.
    Signed-off-by: Mariusz Fik <mariusz@fidano.pl>

---
+++ src/main/java/pl/fidano/java/Richard.java
@@ -0,0 +1,24 @@
+package pl.fidano.java;
+
+import org.javacord.api.DiscordApiBuilder;
+import org.javacord.api.util.logging.ExceptionLogger;
+
+public class Richard {
+
+    final static String DISCORD_TOKEN = System.getenv("DISCORD_TOKEN");
+
+    public static void main(String[] args) {
+
+        if (DISCORD_TOKEN == null || DISCORD_TOKEN.isEmpty()) {
+            System.err.println("Please set DISCORD_TOKEN environment variable first !");
+            return;
+        }
+
+        new DiscordApiBuilder().setToken(DISCORD_TOKEN).login().thenAccept(api -> {
+
+            // Print the invite url of your bot
+            System.out.println("You can invite the bot by using the following url: " + api.createBotInvite());
+
+        }).exceptionally(ExceptionLogger.get());
+    }
+}
